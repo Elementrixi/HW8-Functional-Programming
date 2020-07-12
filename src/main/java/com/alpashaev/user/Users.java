5507e9eccf3d95104e245d52e19e050f185176ac
Elementrixi <67051385+Elementrixi@users.noreply.github.com>
2020-07-12 17:09:18 +0300

    Added hachCode and equals override

--- src/main/java/com/alpashaev/user/Users.java
+++ src/main/java/com/alpashaev/user/Users.java
@@ -1,5 +1,7 @@
 package main.java.com.alpashaev.user;
 
+import java.util.Objects;
+
 public class Users {
     private String name;
     private int age;
@@ -17,6 +19,21 @@ public class Users {
 
     public String getCity() {
         return city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Users users = (Users) o;
+        return age == users.age &&
+                name.equals(users.name) &&
+                city.equals(users.city);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, city);
     }
 
     @Override
