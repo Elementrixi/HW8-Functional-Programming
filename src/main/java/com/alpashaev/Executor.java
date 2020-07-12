f43551c6a163850c5eecf88d0006c7b5055f4f5d
Elementrixi <67051385+Elementrixi@users.noreply.github.com>
2020-07-12 17:00:27 +0300

    Delete Executor.java

--- src/main/java/com/alpashaev/Executor.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.alpashaev;
-
-import static main.java.com.alpashaev.user.UsersOperations.usersListOperations;
-
-public class Executor {
-    public static void homeworkSolution() {
-        usersListOperations();
-    }
-}
